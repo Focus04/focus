71562b66103e825dc172de25030feff9c98853b7
Focus04 <alexogrezeanu2004@gmail.com>
2020-07-10 17:07:26 +0300

    .
    .

--- Commands/userinfo.js
+++ Commands/userinfo.js
@@ -11,7 +11,7 @@ module.exports = {
             message.member.roles.cache.map(role => {
                 roles = roles + role.name + `\n`;
             })
-            roles = roles - 1 + '```';
+            roles = roles - `\n` + '```';
             let perms='```';
             message.member.permissions.toArray().map(perm => {
                 perms = perms + perm + `\n`;
