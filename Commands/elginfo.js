d9844fb1e6c59a0a5c18fbc19d31f94921aaa8d6
Focus04 <alexogrezeanu2004@gmail.com>
2020-07-12 13:39:41 +0300

    .
    .

--- Commands/elginfo.js
+++ Commands/elginfo.js
@@ -2,23 +2,24 @@ const Discord = require('discord.js');
 const moment = require('moment');
 const fetch = require('node-fetch');
 module.exports = {
-    name: 'rginfo',
+    name: 'elginfo',
     description: `Tells you information about your favourite SA-MP community!.`,
-    usage: 'rginfo',
+    usage: 'elginfo',
     guildOnly: true,
     async execute(message, args) {
-        let data = await fetch('https://monitor.teamshrimp.com/api/fetch/all/91.134.166.78/1337/')
+        let data = await fetch('https://monitor.teamshrimp.com/api/fetch/all/87.98.132.123/7777/')
             .then(response => response.json());
         let online;
-        if (data.online)
-            online = 'Yes';
-        else
-            online = 'No';
+        if (!data.online)
+            return message.channel.send('eLg down your lives down.')
         let players = '```';
         data.players.map(player => {
             players = players + `${player.name}(${player.id}) - ${player.score} - ${player.ping}` + `\n`;
         })
-        players = players + '```';
+        if (players === '```')
+            players = '```None```';
+        else
+            players = players + '```';
         let rgembed = new Discord.MessageEmbed()
             .setColor('#00ffbb')
             .setTitle(`${data.servername}`)
@@ -26,13 +27,13 @@ module.exports = {
             .addFields(
                 { name: 'Server IP', value: `${data.ip}:${data.port}`, inline: true },
                 { name: 'Map', value: `${data.mapname}`, inline: true },
-                { name: 'Forums', value: `http://rg-clan.com`, inline: true },
-                { name: 'Online', value: `${online}`, inline: true },
+                { name: 'Time', value: `${data.worldtime}`, inline: true },
+                { name: 'Forums', value: `http://elgclan.net`, inline: true },
                 { name: 'Version', value: `${data.version}`, inline: true },
                 { name: 'Players', value: `${data.num_players}/${data.max_players}`, inline: true },
                 { name: 'Name(ID) - Score - Ping', value: `${players}` }
             )
-            .setThumbnail('https://i.imgur.com/GWRrz6m.png')
+            .setThumbnail('https://images-ext-2.discordapp.net/external/ZrjnUB1D5Q3JPTXvkGZZMnE4NsnOj-PWDrdA9F7brUY/%3Fsize%3D128/https/cdn.discordapp.com/icons/729315570054594632/9c956d6a7384fb330c24d2fffca34eb3.png')
             .setTimestamp();
         message.channel.send(rgembed);
     }
