0d083dcea2df46edc61c278245951bf519f79ee1
Focus04 <alexogrezeanu2004@gmail.com>
2020-07-12 14:27:07 +0300

    .
    .

--- Commands/welcomerole.js
+++ Commands/welcomerole.js
@@ -10,22 +10,21 @@ module.exports = {
     guildOnly: true,
     async execute(message, args) {
         let prefix = await prefixes.get(`${message.guild.id}`);
-        let role = args.join(' ');
-        if(!message.guild.me.hasPermission('MANAGE_ROLES'))
+        let rolename = args.join(' ');
+        if (!message.guild.me.hasPermission('MANAGE_ROLES'))
             return message.channel.send('I need the Manage Roles permission in order to execute this command!');
-        if(!args[0])
+        if (!args[0])
             message.channel.send(`Proper command usage: ${prefix}welcomerole [role]`);
-        else{
-            let rolename = await welcomeroles.get(`welcomerole_${message.guild.id}`);
+        else {
             let welcomerole = message.guild.roles.cache.find(role => role.name === `${rolename}`);
-            if(!welcomerole)
-                message.channel.send(`Couldn't find any roles named "${role}"`);
+            if (!welcomerole)
+                message.channel.send(`Couldn't find any roles named "${rolename}"`);
             else
-                if(!message.member.hasPermission('MANAGE_ROLES'))
+                if (!message.member.hasPermission('MANAGE_ROLES'))
                     message.channel.send('You lack permissions to run this command!');
-                else{
+                else {
                     await welcomeroles.set(`welcomerole_${message.guild.id}`, role);
-                    message.channel.send(`Welcome role successfully changed to ${role}`);
+                    message.channel.send(`Welcome role successfully changed to ${rolename}`);
                 }
         }
     }
