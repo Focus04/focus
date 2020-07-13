81994fe3b11980a04b18687868be752b9625e706
Focus04 <alexogrezeanu2004@gmail.com>
2020-07-13 19:53:36 +0300

    .
    .

--- Commands/giverole.js
+++ Commands/giverole.js
@@ -17,7 +17,7 @@ module.exports = {
         for (let i = 1; i < args.length; i++)
             rolename = rolename + args[i] + ' ';
         if (!message.guild.me.hasPermission('MANAGE_ROLES') || !message.guild.member(member).kickable)
-            return message.channel.send('I need the Manage Roles permission in order to execute this command.');
+            return message.channel.send('I need the Manage Roles permission in order to execute this command.  In case I have it, make sure that my role is higher than the role of the member you want to give a role to!');
         if (!args[1])
             message.channel.send(`Proper command usage: ${prefix}giverole @[member] [role]`);
         else {
@@ -25,38 +25,41 @@ module.exports = {
             if (!role)
                 message.channel.send(`Couldn't find any roles named ${rolename}`);
             else
-                if (!message.member.hasPermission('MANAGE_ROLES'))
-                    message.channel.send('You need the Manage Roles permission in order to run this command!');
-                else {
-                    let highestrole = 1000;
-                    message.member.roles.cache.map(r => {
-                        if (r.position < highestrole)
-                            highestrole = r.position;
-                    });
-                    if (role.position <= highestrole)
-                        message.channel.send('Your roles must be higher than the role that you want to give. In case they are, make sure that my role is higher than the role of the member you want to give a role to!');
+                if(member.roles.cache.has(role.id))
+                    message.channel.send(`${member.username} already has that role.`);
+                else
+                    if (!message.member.hasPermission('MANAGE_ROLES'))
+                        message.channel.send('You need the Manage Roles permission in order to run this command!');
                     else {
-                        member.roles.add(role);
-                        let perms = role.permissions.toArray().map(perm => perm).join(`\n`);
-                        perms = '```' + perms + '```';
-                        let giveroleembed = new Discord.MessageEmbed()
-                            .setColor('#00ffbb')
-                            .setTitle('Given Role')
-                            .addFields(
-                                { name: 'To', value: `${member}` },
-                                { name: 'By', value: `${message.author.username}` },
-                                { name: 'Role', value: `${rolename}` },
-                                { name: 'Permissions', value: `${perms}` }
-                            )
-                            .setTimestamp();
-                        let logchname = await logchannels.get(`logchannel_${message.guild.id}`);
-                        let log = message.guild.channels.cache.find(ch => ch.name === `${logchname}`);
-                        if (log)
-                            log.send(giveroleembed);
-                        else
-                            message.channel.send(giveroleembed);
+                        let highestrole = 1000;
+                        message.member.roles.cache.map(r => {
+                            if (r.position < highestrole)
+                                highestrole = r.position;
+                        });
+                        if (role.position <= highestrole)
+                            message.channel.send('Your roles must be higher than the role that you want to give. In case they are, make sure that my role is higher than the role of the member you want to give a role to!');
+                        else {
+                            member.roles.add(role);
+                            let perms = role.permissions.toArray().map(perm => perm).join(`\n`);
+                            perms = '```' + perms + '```';
+                            let giveroleembed = new Discord.MessageEmbed()
+                                .setColor('#00ffbb')
+                                .setTitle('Given Role')
+                                .addFields(
+                                    { name: 'To', value: `${member}` },
+                                    { name: 'By', value: `${message.author.username}` },
+                                    { name: 'Role', value: `${rolename}` },
+                                    { name: 'Permissions', value: `${perms}` }
+                                )
+                                .setTimestamp();
+                            let logchname = await logchannels.get(`logchannel_${message.guild.id}`);
+                            let log = message.guild.channels.cache.find(ch => ch.name === `${logchname}`);
+                            if (log)
+                                log.send(giveroleembed);
+                            else
+                                message.channel.send(giveroleembed);
+                        }
                     }
-                }
         }
     }
 }
--- Commands/takerole.js
+++ Commands/takerole.js
@@ -17,7 +17,7 @@ module.exports = {
         for (let i = 1; i < args.length; i++)
             rolename = rolename + args[i] + ' ';
         if (!message.guild.me.hasPermission('MANAGE_ROLES') || !message.guild.member(member).kickable)
-            return message.channel.send('I need the Manage Roles permission in order to execute this command.');
+            return message.channel.send('I need the Manage Roles permission in order to execute this command.   In case I have it, make sure that my role is higher than the role of the member you want to give a role to!');
         if (!args[1])
             message.channel.send(`Proper command usage: ${prefix}takerole @[member] [role]`);
         else {
@@ -25,41 +25,38 @@ module.exports = {
             if (!role)
                 message.channel.send(`${member.username} doesn't have any roles named ${rolename}`);
             else
-                if (!member.roles.cache.has(role.id))
-                    message.channel.send(`${member.username} doesn't have the role you specified.`);
-                else
-                    if (!message.member.hasPermission('MANAGE_ROLES'))
-                        message.channel.send('You need the Manage Roles permission in order to run this command!');
+                if (!message.member.hasPermission('MANAGE_ROLES'))
+                    message.channel.send('You need the Manage Roles permission in order to run this command!');
+                else {
+                    let highestrole = 1000;
+                    message.member.roles.cache.map(r => {
+                        if (r.position < highestrole)
+                            highestrole = r.position;
+                    });
+                    if (role.position <= highestrole)
+                        message.channel.send('Your roles must be higher than the role that you want to take. In case I have it, make sure that my role is higher than the role of the member that you want to give a role to!');
                     else {
-                        let highestrole = 1000;
-                        message.member.roles.cache.map(r => {
-                            if (r.position < highestrole)
-                                highestrole = r.position;
-                        });
-                        if (role.position <= highestrole)
-                            message.channel.send('Your roles must be higher than the role that you want to take. In case I have it, make sure that my role is higher than the role of the member that you want to give a role to!');
-                        else {
-                            member.roles.remove(role);
-                            let perms = role.permissions.toArray().map(perm => perm).join(`\n`);
-                            perms = '```' + perms + '```';
-                            let takeroleembed = new Discord.MessageEmbed()
-                                .setColor('#00ffbb')
-                                .setTitle('Deleted Role')
-                                .addFields(
-                                    { name: 'From', value: `${member}` },
-                                    { name: 'By', value: `${message.author.username}` },
-                                    { name: 'Role', value: `${rolename}` },
-                                    { name: 'Permissions', value: `${perms}` }
-                                )
-                                .setTimestamp();
-                            let logchname = await logchannels.get(`logchannel_${message.guild.id}`);
-                            let log = message.guild.channels.cache.find(ch => ch.name === `${logchname}`);
-                            if (log)
-                                log.send(takeroleembed);
-                            else
-                                message.channel.send(takeroleembed);
-                        }
+                        member.roles.remove(role);
+                        let perms = role.permissions.toArray().map(perm => perm).join(`\n`);
+                        perms = '```' + perms + '```';
+                        let takeroleembed = new Discord.MessageEmbed()
+                            .setColor('#00ffbb')
+                            .setTitle('Deleted Role')
+                            .addFields(
+                                { name: 'From', value: `${member}` },
+                                { name: 'By', value: `${message.author.username}` },
+                                { name: 'Role', value: `${rolename}` },
+                                { name: 'Permissions', value: `${perms}` }
+                            )
+                            .setTimestamp();
+                        let logchname = await logchannels.get(`logchannel_${message.guild.id}`);
+                        let log = message.guild.channels.cache.find(ch => ch.name === `${logchname}`);
+                        if (log)
+                            log.send(takeroleembed);
+                        else
+                            message.channel.send(takeroleembed);
                     }
+                }
         }
     }
 }
