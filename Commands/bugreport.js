1524160eb4cc511b310e1cd15fb88114df44c5f5
Focus04 <alexogrezeanu2004@gmail.com>
2020-07-30 15:16:07 +0300

    .
    .

--- Commands/bugreport.js
+++ Commands/bugreport.js
@@ -12,13 +12,13 @@ module.exports = {
         if (!prefix)
             prefix = '/';
         let author = message.author.username;
-        let bug;
+        let bug = [];
         for (let i = 0; i < args.length; i++)
-            bug = bug + ' ' + args[i];
+            bug = bug + args[i] + ' ';
         if (!bug)
             message.channel.send(`Proper command usage: ${prefix}bugreport [bug]. Make sure that you include all the steps needed to reproduce the bug.`);
         else {
-            message.client.channels.cache.get('725434669453279352').send(`__Bug reported by ${author}__\n \n${bug}`);
+            message.client.channels.cache.get('725434669453279352').send(`__Bug reported by ${author}__\n\n${bug}`);
             message.channel.send(`Your bug has been successfully submitted to our server and is now awaiting a review from the developer's side. You can join our Discord server anytime using this link: https://discord.gg/YvN7jUD`);
         }
     }
