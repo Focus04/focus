36103e22793fc4de3c606d8ece3181983b1bf290
Focus04 <alexogrezeanu2004@gmail.com>
2020-08-01 18:30:12 +0300

    .
    .

--- Commands/welcomemessage.js
+++ Commands/welcomemessage.js
@@ -25,22 +25,20 @@ module.exports = {
                 message.channel.send(`You need to set a channel for welcome messages to be sent in. Use ${prefix}setwelcomechannel to setup one.`);
             else {
                 message.channel.send('Input a message. `[user]` will be replaced with a username.');
-                setTimeout(function () {
-                    message.client.on('message', async msg => {
-                        if(msg.author.id == author) {
-                            let welcomemsg = msg.content
-                            await welcomemessages.set(`welcomemessage_${msg.guild.id}`, welcomemsg);
-                            await togglewelcomemsg.set(`togglewelcomemsg_${msg.guild.id}`, 1);
-                            welcomemsg = '`' + welcomemsg + '`';
-                            let logchname = await logchannels.get(`logchannel_${msg.guild.id}`);
-                            let log = msg.guild.channels.cache.find(logchname);
-                            if(!log)
-                                msg.channel.send(`Welcome message successfully changed to ${welcomemsg}`);
-                            else
-                                log.send(`Welcome message successfully changed to ${welcomemsg}`);
-                        }
-                    })
-                }, 60000);
+                message.client.on('message', async msg => {
+                    if(msg.author.id == author) {
+                        let welcomemsg = msg.content
+                        await welcomemessages.set(`welcomemessage_${msg.guild.id}`, welcomemsg);
+                        await togglewelcomemsg.set(`togglewelcomemsg_${msg.guild.id}`, 1);
+                        welcomemsg = '`' + welcomemsg + '`';
+                        let logchname = await logchannels.get(`logchannel_${msg.guild.id}`);
+                        let log = msg.guild.channels.cache.find(logchname);
+                        if(!log)
+                            msg.channel.send(`Welcome message successfully changed to ${welcomemsg}`);
+                        else
+                            log.send(`Welcome message successfully changed to ${welcomemsg}`);
+                    }
+                })
             }
         }
     }
