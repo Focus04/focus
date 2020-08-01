e17c8690950d520458ab293b431e782ab0bc01cc
Focus04 <alexogrezeanu2004@gmail.com>
2020-08-01 20:29:42 +0300

    .
    .

--- Commands/welcomemessage.js
+++ Commands/welcomemessage.js
@@ -10,35 +10,36 @@ const logchannels = new Keyv(database.logchannels);
 module.exports = {
     name: 'welcomemessage',
     description: `Sets a custom welcome message to be displayed when someone joins the server.`,
-    usage: 'welcomemessage',
+    usage: 'welcomemessage [message]',
     guildOnly: true,
-    async execute(message) {
+    async execute(message,args) {
         let prefix = await prefixes.get(message.guild.id);
         if (!prefix)
             prefix = '/';
-        if (!message.member.hasPermission('MANAGE_GUILD'))
-            message.channel.send('You require the Manage Server permission in order to run this command.');
-        else {
-            let welcomechname = await welcomechannels.get(`welcomechannel_${message.guild.id}`);
-            let welcomechannel = await message.guild.channels.cache.find(ch => ch.name === `${welcomechname}`);
-            if (!welcomechannel)
-                message.channel.send(`You need to set a channel for welcome messages to be sent in. Use ${prefix}setwelcomechannel to setup one.`);
+        let msg = [];
+        for (let i = 0; i < args.length; i++)
+            msg = msg + args[i] + ' ';
+        if(!args[0])
+            message.channel.send(`Proper command usage: ${prefix}welcomemessage [message]. Use [user] to be replaced with a username.`);
+        else
+            if (!message.member.hasPermission('MANAGE_GUILD'))
+                message.channel.send('You require the Manage Server permission in order to run this command.');
             else {
-                await message.channel.send('Input a message. `[user]` will be replaced with a username.');
-                client.on('message', async msg => {
-                    if (msg.author.id == message.author.id) {
-                        await welcomemessages.set(`welcomemessage_${msg.guild.id}`, msg.content);
-                        await togglewelcomemsg.set(`togglewelcomemsg_${msg.guild.id}`, 1);
-                        let logchname = await logchannels.get(`logchannel_${msg.guild.id}`);
-                        let log = await msg.guild.channels.cache.find(ch => ch.name === `${logchname}`);
-                        let welcomemsg = '`' + msg.content + '`';
-                        if (!log)
-                            msg.channel.send(`Welcome message successfully changed to ${welcomemsg}`);
-                        else
-                            log.send(`Welcome message successfully changed to ${welcomemsg}`);
-                    }
-                })
+                let welcomechname = await welcomechannels.get(`welcomechannel_${message.guild.id}`);
+                let welcomechannel = await message.guild.channels.cache.find(ch => ch.name === `${welcomechname}`);
+                if (!welcomechannel)
+                    message.channel.send(`You need to set a channel for welcome messages to be sent in. Use ${prefix}setwelcomechannel to setup one.`);
+                else {
+                    await welcomemessages.set(`welcomemessage_${message.guild.id}`, msg);
+                    await togglewelcomemsg.set(`togglewelcomemsg_${message.guild.id}`, 1);
+                    let logchname = await logchannels.get(`logchannel_${message.guild.id}`);
+                    let log = await message.guild.channels.cache.find(ch => ch.name === `${logchname}`);
+                    let welcomemsg = '`' + message.content + '`';
+                    if (!log)
+                        message.channel.send(`Welcome message successfully changed to ${welcomemsg}`);
+                    else
+                        log.send(`Welcome message successfully changed to ${welcomemsg}`);
+                }
             }
-        }
     }
 }
