f2a046f021596713d947116f18c5ffb977c2d7a5
Focus04 <alexogrezeanu2004@gmail.com>
2020-08-03 19:38:40 +0300

    .
    .

--- Commands/ban.js
+++ Commands/ban.js
@@ -32,8 +32,7 @@ module.exports = {
                 else {
                     if (member.id == message.author.id)
                         return message.channel.send(`You can't ban youself, smarty pants!`);
-                    let banneduser = [member.user.username, member.user.id];
-                    await bannedusers.set(`${message.guild.id}`, banneduser);
+                    await bannedusers.set(`${message.guild.id}_${member.user.username}`, member.user.id);
                     let bans = await bns.get(`bans_${member.id}_${message.guild.id}`);
                     if (!bans)
                         bans = 1;
