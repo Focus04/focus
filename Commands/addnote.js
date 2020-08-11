89dd7aeb9361e88840f97c20dd9cade95dce897b
Focus04 <alexogrezeanu2004@gmail.com>
2020-08-11 11:40:14 +0300

    .
    .

--- Commands/addnote.js
+++ Commands/addnote.js
@@ -15,30 +15,26 @@ module.exports = {
             prefix = '/';
         if (!args[1]) {
             message.channel.send(`Proper command usage: ${prefix}addnote [username] [note]`);
-            message.react('❌');
+            return message.react('❌');
         }
-        else {
-            let member = message.guild.members.cache.find(user => user.user.username === `${args[0]}` || user.nickname === `${args[0]}`);
-            if(!member){
-                await message.author.send(`Couldn't find ${args[0]}.`);
-                return message.channel.bulkDelete(1);
-            }
-            if (!message.member.hasPermission('KICK_MEMBERS')) {
-                message.channel.send('You need the Kick Members permission in order to run this command.');
-                message.react('❌');
-            }
-            else {
-                args.shift();
-                let note = '```' + args.join(' ') + '```';
-                let notes = await nts.get(`notes_${member.id}_${message.guild.id}`);
-                if (!notes)
-                    notes = note + `Added by ${message.author.username} on ${moment(message.createdTimestamp).format('LL')}, at ${moment(message.createdTimestamp).format('LT')} GMT\n`;
-                else
-                    notes = notes + note + `Added by ${message.author.username} on ${moment(message.createdTimestamp).format('LL')}, at ${moment(message.createdTimestamp).format('LT')} GMT\n`;
-                await nts.set(`notes_${member.id}_${message.guild.id}`, notes);
-                await message.author.send(`Note successfully added on ${member.user.username}'s account`);
-                message.channel.bulkDelete(1);
-            }
+        let member = message.guild.members.cache.find(user => user.user.username === `${args[0]}` || user.nickname === `${args[0]}`);
+        if(!member){
+            await message.author.send(`Couldn't find ${args[0]}.`);
+            return message.channel.bulkDelete(1);
         }
+        if (!message.member.hasPermission('KICK_MEMBERS')) {
+            message.channel.send('You need the Kick Members permission in order to run this command.');
+            return message.react('❌');
+        }
+        args.shift();
+        let note = '```' + args.join(' ') + '```';
+        let notes = await nts.get(`notes_${member.id}_${message.guild.id}`);
+        if (!notes)
+            notes = note + `Added by ${message.author.username} on ${moment(message.createdTimestamp).format('LL')}, at ${moment(message.createdTimestamp).format('LT')} GMT\n`;
+        else
+            notes = notes + note + `Added by ${message.author.username} on ${moment(message.createdTimestamp).format('LL')}, at ${moment(message.createdTimestamp).format('LT')} GMT\n`;
+        await nts.set(`notes_${member.id}_${message.guild.id}`, notes);
+        await message.author.send(`Note successfully added on ${member.user.username}'s account`);
+         message.channel.bulkDelete(1);
     }
 }
