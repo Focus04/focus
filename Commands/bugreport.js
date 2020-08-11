49e7726a357a8d054c56b7cb949cf9939053d22a
Focus04 <alexogrezeanu2004@gmail.com>
2020-08-11 11:56:06 +0300

    .
    .

--- Commands/bugreport.js
+++ Commands/bugreport.js
@@ -13,14 +13,12 @@ module.exports = {
             prefix = '/';
         let author = message.author.username;
         let bug = '```' + args.join(' ') + '```';
-        if (!bug) {
+        if (!args[0]) {
             message.channel.send(`Proper command usage: ${prefix}bugreport [bug]. Make sure that you include all the steps needed to reproduce the bug.`);
-            message.react('❌');
+            return message.react('❌');
         }
-        else {
-            message.client.channels.cache.get('725434669453279352').send(`__Bug reported by ${author}__\n\n${bug}`);
-            message.react('✔️');
-            message.channel.send(`Your bug has been successfully submitted to our server and is now awaiting a review from the developer's side. You can join our Discord server anytime using this link: https://discord.gg/YvN7jUD`);
-        }
+        message.client.channels.cache.get('725434669453279352').send(`__Bug reported by ${author}__\n\n${bug}`);
+        message.react('✔️');
+        message.channel.send(`Your bug has been successfully submitted to our server and is now awaiting a review from the developer's side. You can join our Discord server anytime using this link: https://discord.gg/YvN7jUD`);
     }
 }
