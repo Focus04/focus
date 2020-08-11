a1713971c959934f9475f8380ae3c5c18b2142a2
Focus04 <alexogrezeanu2004@gmail.com>
2020-08-11 12:08:18 +0300

    .
    .

--- Commands/clear.js
+++ Commands/clear.js
@@ -14,37 +14,33 @@ module.exports = {
         }
         if (!message.member.hasPermission('MANAGE_MESSAGES')) {
             message.channel.send(`It appears that you don't have permission to clear messages.`);
-            message.channel.react('❌');
+            return message.channel.react('❌');
         }
-        else {
-            const amount = parseInt(args[0]) + 1;
-            if (isNaN(amount) || amount < 2 || amount > 100) {
-                message.channel.send(`You must enter a number higher than 0 and less than 100.`);
-                message.react('❌');
-            }
-            else {
-                message.channel.bulkDelete(amount, true).catch(err => {
-                    console.error(err);
-                    message.channel.send(`Can't delete messages older than 2 weeks.`);
-                    return message.react('❌');
-                });
-                let clearembed = new Discord.MessageEmbed()
-                    .setColor('#00ffbb')
-                    .setTitle(`${message.client.emojis.cache.find(emoji => emoji.name === 'pinned')} Cleared Messages`)
-                    .addFields(
-                        { name: 'Cleared by:', value: `${message.author.username}` },
-                        { name: 'Amount of Messages Deleted:', value: `${amount}` },
-                        { name: 'Channel:', value: `${message.channel.name}` }
-                    )
-                    .setTimestamp();
-                message.react('✔️');
-                let logchname = await logchannels.get(`logchannel_${message.guild.id}`);
-                let log = message.guild.channels.cache.find(ch => ch.name === `${logchname}`);
-                if (!log)
-                    message.channel.send(clearembed);
-                else
-                    log.send(clearembed);
-            }
+        const amount = parseInt(args[0]) + 1;
+        if (isNaN(amount) || amount < 2 || amount > 100) {
+            message.channel.send(`You must enter a number higher than 0 and less than 100.`);
+            return message.react('❌');
         }
+        message.channel.bulkDelete(amount, true).catch(err => {
+            console.error(err);
+            message.channel.send(`Can't delete messages older than 2 weeks.`);
+            return message.react('❌');
+        });
+        let clearembed = new Discord.MessageEmbed()
+            .setColor('#00ffbb')
+            .setTitle(`${message.client.emojis.cache.find(emoji => emoji.name === 'pinned')} Cleared Messages`)
+            .addFields(
+                { name: 'Cleared by:', value: `${message.author.username}` },
+                { name: 'Amount of Messages Deleted:', value: `${amount}` },
+                { name: 'Channel:', value: `${message.channel.name}` }
+            )
+            .setTimestamp();
+        message.react('✔️');
+        let logchname = await logchannels.get(`logchannel_${message.guild.id}`);
+        let log = message.guild.channels.cache.find(ch => ch.name === `${logchname}`);
+        if (!log)
+            message.channel.send(clearembed);
+        else
+            log.send(clearembed);
     }
 }
