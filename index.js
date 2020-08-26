e0b6ce5aabd7e4fa66638e9cd8f2c01c39373253
Focus04 <alexogrezeanu2004@gmail.com>
2020-08-26 19:17:34 +0300

    .

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const Keyv = require('keyv');
-const svvars = new Keyv(process.env.svvars);
+const prefixes = new Keyv(process.env.prefixes);
+const logchannels = new Keyv(process.env.logchannels);
+const msglogs = new Keyv(process.env.msglogs);
+const welcomechannels = new Keyv(process.env.welcomechannels);
+const welcomeroles = new Keyv(process.env.welcomeroles);
+const welcomemessages = new Keyv(process.env.welcomemessages);
+const togglewelcome = new Keyv(process.env.togglewelcome);
+const welcomedms = new Keyv(process.env.welcomedms);
+const togglewelcomedm = new Keyv(process.env.togglewelcomedm)
+const leavechannels = new Keyv(process.env.leavechannels);
+const leavemessages = new Keyv(process.env.leavemessages);
+const toggleleave = new Keyv(process.env.toggleleave);
 const client = new Discord.Client();
 
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./Commands');
-commandFiles.forEach (file => {
+commandFiles.forEach(file => {
     const command = require(`./Commands/${file}`);
     client.commands.set(command.name, command);
 });
@@ -16,42 +27,25 @@ client.on('ready', () => {
     client.user.setActivity('your people.', { type: 'WATCHING' });
 })
 
-client.on('guildCreate', async guild => {
+client.on('guildCreate', () => {
     client.user.setActivity('your people.', { type: 'WATCHING' });
-    const vars = {
-        prefix: "",
-        welcomechannel: "",
-        leavechannel: "",
-        logchannel: "",
-        welcomemsg: "",
-        welcomedm: "",
-        welcomerole: "",
-        leavemsg: "",
-        togglewelcome: 1,
-        toggleleave: 1,
-        togglewelcomedm: 1,
-        togglemsglogs: 0
-    };
-    await svvars.set(guild.id, vars);
-    
 })
 
 client.on('guildMemberAdd', async member => {
-    let db = await svvars.get(member.guild.id);
-    let welcomechname = db.welcomechannel;
+    let welcomechname = await welcomechannels.get(`welcomechannel_${member.guild.id}`);
     let welcome = member.guild.channels.cache.find(ch => ch.name === welcomechname);
-    let dm = db.welcomedm;
-    let dmstate = db.togglewelcomedm;
+    let dm = await welcomedms.get(`welcomedm_${member.guild.id}`);
+    let dmstate = await togglewelcomedm.get(`togglewelcomedm_${member.guild.id}`) || 1;
     if (dm && dmstate == 1)
         member.send(dm.replace('[user]', `${member.user.username}`));
-    let welcomerolename = db.welcomerole;
+    let welcomerolename = await welcomeroles.get(`welcomerole_${member.guild.id}`);
     let welcomerole = member.guild.roles.cache.find(role => role.name === `${welcomerolename}`);
     if (welcomerole)
         member.roles.add(welcomerole);
-    let state = db.togglewelcome;
+    let state = await togglewelcome.get(`togglewelcomemsg_${member.guild.id}`) || 1;
     if (welcome && state == 1) {
         let msg;
-        let welcomemessage = db.welcomemsg;
+        let welcomemessage = await welcomemessages.get(`welcomemessage_${member.guild.id}`);
         if (!welcomemessage)
             msg = `Wish ${member} a pleasant stay!`;
         else
@@ -61,13 +55,12 @@ client.on('guildMemberAdd', async member => {
 })
 
 client.on('guildMemberRemove', async member => {
-    let db = await svvars.get(member.guild.id);
-    let leavechname = db.leavechannel;
+    let leavechname = await leavechannels.get(`leavechannel_${member.guild.id}`);
     let leave = member.guild.channels.cache.find(ch => ch.name === leavechname);
-    let state = db.toggleleave;
+    let state = await toggleleave.get(`toggleleavemsg_${member.guild.id}`) || 1;
     if (leave && state == 1) {
         let msg;
-        let leavemessage = db.leavemsg;
+        let leavemessage = await leavemessages.get(`leavemessage_${member.guild.id}`);
         if (!leavemessage)
             msg = `${member.user.username} has parted ways with us...`;
         else
@@ -77,14 +70,13 @@ client.on('guildMemberRemove', async member => {
 })
 
 client.on('messageDelete', async message => {
-    let db = await svvars.get(message.guild.id);
-    let logchname = db.logchannel;
+    let logchname = await logchannels.get(`logchannel_${message.guild.id}`);
     let log = message.guild.channels.cache.find(ch => ch.name === `${logchname}`);
-    let msglog = db.togglemsglogs;
+    let msglog = await msglogs.get(`msglogs_${message.guild.id}`);
     if (log && msglog == 1 && !message.author.bot) {
         let deleteembed = new Discord.MessageEmbed()
             .setColor('#00ffbb')
-            .setTitle(`${client.emojis.cache.find(emoji => emoji.name === 'pinned')} Message Deleted`)
+            .setTitle(`${message.client.emojis.cache.find(emoji => emoji.name === 'pinned')} Message Deleted`)
             .addFields(
                 { name: 'Author:', value: `${message.author.username}` },
                 { name: 'Channel:', value: `${message.channel.name}` },
@@ -96,14 +88,13 @@ client.on('messageDelete', async message => {
 })
 
 client.on('messageUpdate', async (oldmsg, newmsg) => {
-    let db = await svvars.get(oldmsg.guild.id);
-    let logchname = db.logchannel;
+    let logchname = await logchannels.get(`logchannel_${oldmsg.guild.id}`);
     let log = oldmsg.guild.channels.cache.find(ch => ch.name === `${logchname}`);
-    let msglog = db.togglemsglogs;
+    let msglog = await msglogs.get(`msglogs_${oldmsg.guild.id}`);
     if (log && msglog == 1 && !oldmsg.author.bot) {
         let editembed = new Discord.MessageEmbed()
             .setColor('#00ffbb')
-            .setTitle(`${client.emojis.cache.find(emoji => emoji.name === 'pinned')} Message Edited`)
+            .setTitle(`${message.client.emojis.cache.find(emoji => emoji.name === 'pinned')} Message Edited`)
             .addFields(
                 { name: 'Author:', value: `${oldmsg.author.username}` },
                 { name: 'Channel:', value: `${oldmsg.channel.name}` },
@@ -116,14 +107,15 @@ client.on('messageUpdate', async (oldmsg, newmsg) => {
 })
 
 client.on('message', async message => {
-    let db = await svvars.get(message.guild.id);
     let prefix = "/";
-    let customprefix = db.prefix;
+    let customprefix = await prefixes.get(`${message.guild.id}`);
     if (customprefix)
         prefix = customprefix;
     if (!message.content.startsWith(prefix) || message.author.bot)
         return;
-    const args = message.content.slice(prefix.length).split(/ +/);
+    const args = message.content
+        .slice(prefix.length)
+        .split(/ +/);
     const command = client.commands.get(args.shift().toLowerCase());
     if (!command)
         return;
