3b2b214a66a99365933044a72ea5bec3d1dd610d
Focus04 <alexogrezeanu2004@gmail.com>
2020-08-27 15:00:30 +0300

    Final Touch

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const Keyv = require('keyv');
-const prefixes = new Keyv(process.env.prefixes);
-const logchannels = new Keyv(process.env.logchannels);
-const msglogs = new Keyv(process.env.msglogs);
-const welcomechannels = new Keyv(process.env.welcomechannels);
-const welcomeroles = new Keyv(process.env.welcomeroles);
-const welcomemessages = new Keyv(process.env.welcomemessages);
-const togglewelcome = new Keyv(process.env.togglewelcome);
-const welcomedms = new Keyv(process.env.welcomedms);
-const togglewelcomedm = new Keyv(process.env.togglewelcomedm);
-const leavechannels = new Keyv(process.env.leavechannels);
-const leavemessages = new Keyv(process.env.leavemessages);
-const toggleleave = new Keyv(process.env.toggleleave);
 const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
