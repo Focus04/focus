c8a8955763991af1a6f8988a7b158cf3e784cc72
Focus04 <alexogrezeanu2004@gmail.com>
2020-08-29 17:55:02 +0300

    Improved /define reliability

--- Commands/Fun/define.js
+++ Commands/Fun/define.js
@@ -13,24 +13,26 @@ module.exports = {
             return message.react('❌');
         }
         let term = args.join(' ');
-        let data = await fetch(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${term}?key=abef1297-7c98-456d-8682-4b605eb9c29b`)
-            .then(res => res.json());
+        let response = await fetch(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${term}?key=abef1297-7c98-456d-8682-4b605eb9c29b`)
+        let data = await response.json();
         if (!data[0]) {
             let msg = await message.channel.send(`Couldn't find any results for ${term}`);
             msg.delete({timeout: 10000});
             return message.react('❌');
         }
-        let synonyms = '```';
-        data[0].meta.syns[0].map(syn => {
-            synonyms = synonyms + syn + ', ';
-        })
-        synonyms = synonyms + '```';
+        if(data[0].meta.syns[0]) {
+            let synonyms = '```';
+            data[0].meta.syns[0].map(syn => {
+                synonyms = synonyms + syn + ', ';
+            })
+            synonyms = synonyms + '```';
+        }
         let defineembed = new Discord.MessageEmbed()
             .setColor('#00ffbb')
             .setTitle(`What does ${args[0]} mean?`)
             .addFields(
                 { name: 'Definition', value: `${data[0].def[0].sseq[0][0][1].dt[0][1]}` },
-                { name: 'Synonyms', value: `${synonyms}` }
+                { name: 'Synonyms', value: `${synonyms || 'N/A'}` }
             )
             .setTimestamp();
         message.react('✔️');
