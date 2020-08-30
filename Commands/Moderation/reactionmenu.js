f78cdb76d13cc3751218388676488a4f8f593057
Focus04 <alexogrezeanu2004@gmail.com>
2020-08-30 16:40:15 +0300

    .

--- Commands/Moderation/reactionmenu.js
+++ Commands/Moderation/reactionmenu.js
@@ -1,5 +1,3 @@
-const { execute } = require("../Fun/define");
-
 module.exports = {
     name: 'reactionmenu',
     description: 'Creates a menu that automatically assigns roles to users that react to it.',
@@ -9,11 +7,11 @@ module.exports = {
         let emojis, roles;
         if(!args[1]) {
             let msg = await message.channel.send(`Proper command usage: ${prefix}reactionmenu [emoji] [role] [emoji] [role] etc.`);
-            await msg.delete({timeout: 10000});
+            msg.delete({timeout: 10000});
             return message.react('❌');
         }
         args.forEach(arg => {
-            if(arg % 2 == 0)
+            if(args.indexOf(arg) % 2 == 0)
                 emojis.push(arg);
             else
                 roles.push(arg);
