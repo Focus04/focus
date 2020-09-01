d6aabf0e50d8ad155d839b2fb179a5e0216ecee8
Focus04 <alexogrezeanu2004@gmail.com>
2020-09-01 19:52:46 +0300

    .

--- Commands/Moderation/rolepicker.js
+++ Commands/Moderation/rolepicker.js
@@ -8,6 +8,7 @@ module.exports = {
     async execute(message, args, prefix) {
         let roles = [];
         let emojis = [];
+        let err = 0;
         args.forEach(async arg => {
             if (args.indexOf(arg) % 2 == 0 && arg.startsWith('<@&')) {
                 let role = message.guild.roles.cache.get(arg.substring(3, 21));
@@ -17,7 +18,8 @@ module.exports = {
                         bothighestrole = r.position;
                 })
                 if (role.position >= bothighestrole) {
-                    let msg = await message.channel.send(`Error at role ${role.name}. My roles must be higher than the role that you want to set.`);
+                    err = 1;
+                    let msg = await message.channel.send(`Error at role ${'`' + role.name + '`'}. My roles must be higher than the role that you want to set.`);
                     return msg.delete({ timeout: 10000 });
                 }
                 let highestrole = -1;
@@ -26,7 +28,8 @@ module.exports = {
                         highestrole = r.position;
                 });
                 if (role.position >= highestrole) {
-                    let msg = await message.channel.send(`Error at role ${role.name}. Your roles must be higher than the role that you want to set.`);
+                    err = 1;
+                    let msg = await message.channel.send(`Error at role ${'`' + role.name + '`'}. Your roles must be higher than the role that you want to set.`);
                     return msg.delete({ timeout: 10000 });
                 }
                 roles.push(role);
@@ -34,6 +37,8 @@ module.exports = {
             if (args.indexOf(arg) % 2 == 1 && !arg.startsWith('<@&'))
                 emojis.push(arg);
         });
+        if (err == 1)
+            return;
         if (!message.guild.me.hasPermission('MANAGE_ROLES')) {
             let msg = await message.channel.send('I require the Manage Roles permission in order to execute this command.');
             return msg.delete({ timeout: 10000 });
