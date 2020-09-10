f0568a41d0e2985a47c6cb95255539797bc634c6
Focus04 <alexogrezeanu2004@gmail.com>
2020-09-10 16:12:54 +0300

    Fixed /nasanews

---
+++ Commands/Fun/nasanews.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+
+module.exports = {
+    name: 'nasanews',
+    description: `Looks up an astronomy-related term on NASA's Website and returns a fact about it.`,
+    usage: 'nasanews `term`',
+    guildOnly: true,
+    async execute(message, args, prefix) {
+        if (!args[0]) {
+            let msg  = await message.channel.send(`Proper command usage: ${prefix}nasanews [term]`);
+            msg.delete({timeout: 10000});
+            return message.react('❌');
+        }
+        let term = args.join(' ');
+        let response = await fetch(`https://images-api.nasa.gov/search?q=${term}`);
+        let data = await response.json();
+        if (!data.collection.items[0].data[0].description) {
+            let msg = await message.channel.send(`Couldn't find any results for ${term}`);
+            msg.delete({timeout: 10000});
+            return message.react('❌');
+        }
+        let nasasearchembed = new Discord.MessageEmbed()
+            .setColor('#00ffbb')
+            .setTitle(data.collection.items[0].data[0].title)
+            .setDescription(data.collection.items[0].data[0].description)
+            .setImage(data.collection.items[0].links[0].href.replace(' ', '%20'))
+            .setTimestamp();
+        await message.channel.send(nasasearchembed);
+        message.react('✔️');
+    }
+}
