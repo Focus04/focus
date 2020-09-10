fc4e19d5a1e81b6f1e220f6c326038b279bd2970
Focus04 <alexogrezeanu2004@gmail.com>
2020-09-10 16:19:23 +0300

    .

--- Commands/Fun/nasanews.js
+++ Commands/Fun/nasanews.js
@@ -26,7 +26,7 @@ module.exports = {
             .setDescription(data.collection.items[0].data[0].description)
             .setImage(data.collection.items[0].links[0].href.replace(' ', '%20'))
             .setTimestamp();
-        console.log(data.collection.items[0].links[0].href.replace(' ', '%20'));
+        console.log(data.collection.items[0].links[0].href.replaceAll(' ', '%20'));
         await message.channel.send(nasasearchembed);
         message.react('✔️');
     }
