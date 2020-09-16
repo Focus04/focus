294f454ccd166ab2418eb9b5a8818b472cbfd053
Focus04 <alexogrezeanu2004@gmail.com>
2020-09-16 17:08:19 +0300

    .

--- Events/Messages/message.js
+++ Events/Messages/message.js
@@ -2,19 +2,14 @@ const Keyv = require('keyv');
 const prefixes = new Keyv(process.env.prefixes);
 
 module.exports = async (client, message) => {
-    if(message.author.bot)
+    if(message.author.bot || message.channel.type !== 'text')
         return;
-    let prefix = "/";
-    let customprefix = await prefixes.get(`${message.guild.id}`);
-    if (customprefix)
-        prefix = customprefix;
+    let prefix = await prefixes.get(`${message.guild.id}`) || '/';
     if (!message.content.startsWith(prefix))
         return;
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = client.commands.get(args.shift().toLowerCase());
     if (!command)
         return;
-    if (command.guildOnly && message.channel.type !== 'text')
-        return message.reply('Nice attempt to slide into my DMs but no thanks! ;)');
     command.execute(message, args, prefix);
 }
