56c69bade2d8812c733ded72e9263760d04ab3ce
ToledoSDL <65132614+ToledoSDL@users.noreply.github.com>
2020-09-22 09:04:42 -0300

    code refactoring

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const fs = require('fs');
-const Discord = require('discord.js');
-const client = new Discord.Client({partials: ['MESSAGE', 'REACTION']});
+const fs = require('fs')
+const Discord = require('discord.js')
+const client = new Discord.Client({ partials: ['MESSAGE', 'REACTION'] })
 
-client.commands = new Discord.Collection();
+client.commands = new Discord.Collection()
 fs.readdirSync('./Commands').forEach(folder => {
-    fs.readdirSync(`./Commands/${folder}`).forEach(file => {
-        const command = require(`./Commands/${folder}/${file}`);
-        client.commands.set(command.name, command);
-    });
-});
+  fs.readdirSync(`./Commands/${folder}`).forEach(file => {
+    const command = require(`./Commands/${folder}/${file}`)
+    client.commands.set(command.name, command)
+  })
+})
 
 fs.readdirSync('./Events').forEach(folder => {
-    fs.readdirSync(`./Events/${folder}`).forEach(file => {
-        const event = require(`./Events/${folder}/${file}`);
-        client.on(file.split('.')[0], event.bind(null, client));
-    });
-});
+  fs.readdirSync(`./Events/${folder}`).forEach(file => {
+    const event = require(`./Events/${folder}/${file}`)
+    client.on(file.split('.')[0], event.bind(null, client))
+  })
+})
 
-client.login(process.env.token);
+client.login(process.env.token)
