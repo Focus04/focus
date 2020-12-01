82f778cfa7b7446ec2dfb76e58da6661f0634213
Focus04 <alexogrezeanu2004@gmail.com>
2020-12-01 13:01:26 +0200

    Added /remindme

---
+++ Commands/Moderation/remindme.js
@@ -0,0 +1,20 @@
+const { reactionError, reactionSuccess, deletionTimeout } = require('../../config.json');
+
+module.exports = {
+  name: 'remindme',
+  description: 'Sets a timer for a reminder.',
+  usage: 'remindme `days` `text`',
+  async execute (message, args, prefix) {
+    if (args[1] || isNaN(args[0])) {
+      let msg = await message.channel.send(`Proper command usage: ${prefix}remindme [days] [text]`);
+      msg.delete({ timeout: deletionTimeout });
+      message.react(reactionError);
+    }
+
+    const days = args[0];
+    args.shift();
+    message.channel.send(`Reminder successfully set for ${days} days from now.`);
+    message.react(reactionSuccess);
+    setTimeout(() => message.author.send(`⏰ Time to ${'`' + args.join(' ') + '`'}`), days * 86400000);
+  }
+}
