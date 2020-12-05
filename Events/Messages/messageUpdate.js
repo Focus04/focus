5b9877f70078582f7f62061932f81981cd0f9ba0
Focus04 <alexogrezeanu2004@gmail.com>
2020-12-05 13:45:13 +0200

    .

--- Events/Messages/messageUpdate.js
+++ Events/Messages/messageUpdate.js
@@ -5,17 +5,18 @@ const msgLogs = new Keyv(process.env.msgLogs);
 const { pinEmojiId } = require('../../config.json');
 
 module.exports = async (client, oldmsg, newmsg) => {
-  if (oldmsg.channel.type !== 'text' || oldmsg.attachments) return;
-  const logChName = await logChannels.get(`logchannel_${oldmsg.guild.id}`);
-  const log = oldmsg.guild.channels.cache.find((ch) => ch.name === `${logChName}`);
-  const msgLog = await msgLogs.get(`msglogs_${oldmsg.guild.id}`);
-  if (log && msgLog == 1 && !oldmsg.author.bot && oldmsg.content.length <= 1024 && newmsg.content.length <= 1024) {
+  if (newmsg.channel.type !== 'text' || newmsg.attachments) return;
+  console.log('Message edited');
+  const logChName = await logChannels.get(`logchannel_${newmsg.guild.id}`);
+  const log = newmsg.guild.channels.cache.find((ch) => ch.name === `${logChName}`);
+  const msgLog = await msgLogs.get(`msglogs_${newmsg.guild.id}`);
+  if (log && msgLog == 1 && !newmsg.author.bot && oldmsg.content.length <= 1024 && newmsg.content.length <= 1024) {
     const editEmbed = new Discord.MessageEmbed()
       .setColor('#00ffbb')
-      .setTitle(`${oldmsg.client.emojis.cache.get(pinEmojiId).toString()} Message Edited`)
+      .setTitle(`${newmsg.client.emojis.cache.get(pinEmojiId).toString()} Message Edited`)
       .addFields(
-        { name: 'Author:', value: `${oldmsg.author.username}` },
-        { name: 'Channel:', value: `${oldmsg.channel.name}` },
+        { name: 'Author:', value: `${newmsg.author.username}` },
+        { name: 'Channel:', value: `${newmsg.channel.name}` },
         { name: 'Initial Content:', value: `${oldmsg.content}` },
         { name: 'New Content:', value: `${newmsg.content}` }
       )
