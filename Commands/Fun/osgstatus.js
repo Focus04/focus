1667ea034b7f779ad0251e025f9240b93667795d
Focus04 <alexogrezeanu2004@gmail.com>
2020-12-05 12:50:23 +0200

    .

--- Commands/Fun/osgstatus.js
+++ Commands/Fun/osgstatus.js
@@ -25,17 +25,16 @@ module.exports = {
     let rgembed = new Discord.MessageEmbed()
       .setColor('#00ffbb')
       .setTitle(`${data.servername}`)
-      .setDescription(`${data.gametype}`)
       .addFields(
         { name: 'Server IP', value: `${data.ip}:${data.port}`, inline: true },
         { name: 'Map', value: `${data.mapname}`, inline: true },
         { name: 'Time', value: `${data.worldtime}`, inline: true },
-        { name: 'Forums', value: `http://elgclan.net`, inline: true },
+        { name: 'Forums', value: 'https://osgclan.net/', inline: true },
         { name: 'Version', value: `${data.version}`, inline: true },
         { name: 'Players', value: `${data.num_players}/${data.max_players}`, inline: true },
         { name: 'Name(ID) - Score - Ping', value: `${players}` }
       )
-      .setThumbnail('https://images-ext-1.discordapp.net/external/rzBGphe1bc-RWrbHzLQdbeSigBFw40lwLJS3PoppyCU/%3Fsize%3D1024/https/cdn.discordapp.com/icons/783743580119236709/29b4b6388600e4843c7bb52e721bec03.webp?width=475&height=475.png')
+      .setThumbnail('https://images-ext-1.discordapp.net/external/rzBGphe1bc-RWrbHzLQdbeSigBFw40lwLJS3PoppyCU/%3Fsize%3D1024/https/cdn.discordapp.com/icons/783743580119236709/29b4b6388600e4843c7bb52e721bec03.webp?width=475&height=475')
       .setTimestamp();
     await message.channel.send(rgembed);
     message.react(reactionSuccess);
