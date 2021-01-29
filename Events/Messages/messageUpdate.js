2b49eaa87cba3e30fde3f1b41058041c8250d35b
Focus04 <alexogrezeanu2004@gmail.com>
2021-01-29 12:55:44 +0200

    Fixed the bot sending logs when sending a url

--- Events/Messages/messageUpdate.js
+++ Events/Messages/messageUpdate.js
@@ -5,7 +5,7 @@ const msgLogs = new Keyv(process.env.msgLogs);
 const { pinEmojiId } = require('../../config.json');
 
 module.exports = async (client, oldmsg, newmsg) => {
-  if (newmsg.channel.type !== 'text' || oldmsg.attachments[0]) return;
+  if (newmsg.channel.type !== 'text' || !newmsg.editedTimestamp) return;
   const logChName = await logChannels.get(`logchannel_${newmsg.guild.id}`);
   const log = newmsg.guild.channels.cache.find((ch) => ch.name === `${logChName}`);
   const msgLog = await msgLogs.get(`msglogs_${newmsg.guild.id}`);
