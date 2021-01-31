952770beb9d7b8db0235f052c4eb40ed9c177e36
Focus04 <alexogrezeanu2004@gmail.com>
2021-01-31 10:44:38 +0200

    .

--- Events/Messages/messageDelete.js
+++ Events/Messages/messageDelete.js
@@ -5,18 +5,19 @@ const msglogs = new Keyv(process.env.msgLogs);
 const { pinEmojiId } = require('../../config.json');
 
 module.exports = async (client, message) => {
-  if (message.channel.type !== 'text') return;
+  if (message.channel.type !== 'text' || message.author.bot) return;
+  if (message.content.length > 1024) return;
   const logChName = await logChannels.get(`logchannel_${message.guild.id}`);
   const log = message.guild.channels.cache.find((ch) => ch.name === `${logChName}`);
   const msgLog = await msglogs.get(`msglogs_${message.guild.id}`);
-  if (log && msgLog == 1 && !message.author.bot && message.content.length <= 1024) {
+  if (log && msgLog == 1) {
     const deleteEmbed = new Discord.MessageEmbed()
-      .setColor('#00ffbb')
+      .setColor(message.guild.me.roles.highest.color)
       .setTitle(`${message.client.emojis.cache.get(pinEmojiId).toString()} Message Deleted`)
       .addFields(
-        { name: 'Author:', value: `${message.author.username}` },
-        { name: 'Channel:', value: `${message.channel.name}` },
-        { name: 'Content:', value: `${message.content}` }
+        { name: 'Author:', value: message.member },
+        { name: 'Channel:', value: `#${message.channel.name}` },
+        { name: 'Content:', value: '```' + message.conent + '```' }
       )
       .setTimestamp();
     log.send(deleteEmbed);
--- Events/Messages/messageUpdate.js
+++ Events/Messages/messageUpdate.js
@@ -5,19 +5,20 @@ const msgLogs = new Keyv(process.env.msgLogs);
 const { pinEmojiId } = require('../../config.json');
 
 module.exports = async (client, oldmsg, newmsg) => {
-  if (newmsg.channel.type !== 'text' || !newmsg.editedTimestamp) return;
+  if (!newmsg.editedTimestamp || newmsg.author.bot) return;
+  if (oldmsg.content.length > 1024 || newmsg.content.length > 1024) return;
   const logChName = await logChannels.get(`logchannel_${newmsg.guild.id}`);
   const log = newmsg.guild.channels.cache.find((ch) => ch.name === `${logChName}`);
   const msgLog = await msgLogs.get(`msglogs_${newmsg.guild.id}`);
-  if (log && msgLog == 1 && !newmsg.author.bot && oldmsg.content.length <= 1024 && newmsg.content.length <= 1024) {
+  if (log && msgLog == 1) {
     const editEmbed = new Discord.MessageEmbed()
-      .setColor('#00ffbb')
+      .setColor(newmsg.guild.me.roles.highest.color)
       .setTitle(`${newmsg.client.emojis.cache.get(pinEmojiId).toString()} Message Edited`)
       .addFields(
-        { name: 'Author:', value: `${newmsg.author.username}` },
-        { name: 'Channel:', value: `${newmsg.channel.name}` },
-        { name: 'Initial Content:', value: `${oldmsg.content}` },
-        { name: 'New Content:', value: `${newmsg.content}` }
+        { name: 'Author:', value: newmsg.member },
+        { name: 'Channel:', value: `#${newmsg.channel.name}` },
+        { name: 'Initial Content:', value: '```' + oldmsg.content + '```' },
+        { name: 'New Content:', value: '```' + newmsg.content + '```' }
       )
       .setTimestamp();
     log.send(editEmbed);
