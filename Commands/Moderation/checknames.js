8e97b89743174674f25dec7ecb2d3a8a3660b919
Focus04 <alexogrezeanu2004@gmail.com>
2021-02-02 22:37:43 +0200

    .

---
+++ Commands/Moderation/checknames.js
@@ -0,0 +1,47 @@
+const { MessageEmbed } = require('discord.js');
+const moment = require('moment');
+const Keyv = requrie('keyv');
+const names = new Keyv(process.env.names);
+const { deletionTimeout, reactionError, reactionSuccess } = require('../../config.json');
+
+module.exports = {
+  name: 'checknames',
+  description: `Check a member's previous nicknames.`,
+  usage: 'checknames @`member`',
+  requiredPerms: 'KICK_MEMBERS',
+  permError: 'You require the Kick Members permission in order to run this command.',
+  async execute(message, args, prefix) {
+    const member = message.mentions.members.first();
+    if (!args[0]) {
+      let msg = await message.channel.send(`Proper command usage: ${prefix}checknames @[member]`);
+      msg.delete({ timeout: deletionTimeout });
+      return message.react(reactionError);
+    }
+
+    if (!member) {
+      let msg = await message.channel.send(`Couldn't find ${member}`);
+      msg.delete({ timeout: deletionTimeout });
+      return message.react(reactionError);
+    }
+
+    const namesArr = await names.get(`${member.user.id}_${message.guild.id}`);
+    if (!namesArr) {
+      let msg = await message.channel.send(`There aren't any name changes logged from this member`);
+      msg.delete({ timeout: deletionTimeout });
+      return message.react(reactionError);
+    }
+
+    let content = '';
+    namesArr.forEach((name) => content += `${'`' + name.nickname + '`'} - ${moment(name.time).format('LL')}, at ${moment(name.time).format('LT')} GMT`);
+    let color;
+    if (message.guild.me.roles.highest.color === 0) color = '#b9bbbe';
+    else color = message.guild.me.roles.highest.color;
+    const nameChangesEmbed = new MessageEmbed()
+      .setColor(color)
+      .setTitle(`${member}'s nicknames`)
+      .setDescription(content)
+      .setTimestamp();
+    await message.channel.send(viewNotesEmbed);
+    message.react(reactionSuccess);
+  }
+}
