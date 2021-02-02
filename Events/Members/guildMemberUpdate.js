25fe6e195132973d66df5ba3f241b10a8703756a
Focus04 <alexogrezeanu2004@gmail.com>
2021-02-02 22:05:54 +0200

    .

--- Events/Members/guildMemberUpdate.js
+++ Events/Members/guildMemberUpdate.js
@@ -2,6 +2,7 @@ const Keyv = require('keyv');
 const names = new Keyv(process.env.names);
 
 module.exports = async (client, oldMember, newMember) => {
+  console.log('guildMemberUpdate triggered.');
   if (oldMember.nickname === newMember.nickname) return;
   console.log(`${oldMember.nickname} changed his nickname to ${newMember.nickname}`);
   const namesArr = await names.get(`${oldMember.user.id}_${oldMember.guild.id}`);
