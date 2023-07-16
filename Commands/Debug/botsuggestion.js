b27f4af654929482ebaba2c1cde0778ea5131e80
Saunak <76943154+Saunakghosh10@users.noreply.github.com>
2023-07-17 00:04:48 +0530

    Add botsuggestion command

--- Commands/Debug/botsuggestion.js
+++ Commands/Debug/botsuggestion.js
@@ -11,11 +11,22 @@ module.exports = {
       .setRequired(true)
     ),
   async execute(interaction) {
-    const author = interaction.member.user.tag;
-    const suggestion = interaction.options.getString('suggestion');
-    await interaction.client.channels.cache
-      .get(suggestionChId)
-      .send(`__Suggestion by ${author}__\n\n${suggestion}`);
-    interaction.reply({ content: `Your suggestion has been successfully submitted to our server and is now awaiting a review from the developer's side. You can join our Discord server anytime using this link: ${discordInviteLink}`, ephemeral: true });
+    try {
+      const author = interaction.member.user.tag;
+      const suggestion = interaction.options.getString('suggestion');
+      await interaction.client.channels.cache
+        .get(suggestionChId)
+        .send(`__Suggestion by ${author}__\n\n${suggestion}`);
+      await interaction.reply({
+        content: `Your suggestion has been successfully submitted to our server and is now awaiting a review from the developer's side. You can join our Discord server anytime using this link: ${discordInviteLink}`,
+        ephemeral: true
+      });
+    } catch (error) {
+      console.error('Error submitting suggestion:', error);
+      await interaction.reply({
+        content: 'An error occurred while submitting your suggestion. Please try again later.',
+        ephemeral: true
+      });
+    }
   }
-}
+};
